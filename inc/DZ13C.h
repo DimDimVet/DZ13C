08d50fc093abf90f9a9ca3dc056c9365a89461be
ДимДим <1000rub@list.ru>
2024-12-17 15:05:12 +0300

    work171224

--- inc/DZ13C.h
+++ inc/DZ13C.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
 #ifndef DZ13C_H_
 #define DZ13C_H_
@@ -10,5 +11,6 @@ int WriteSize();
 void FillArrSinus(int size, double *arr);
 void FillArrNoiseSinus(int size, double *arr, double *noiseArr);
 void PrintConsole(int size, double *arr, char *str);
+void PrintConsoleGraph(int size,double *arr);
 
 #endif
--- src/DZ13C.c
+++ src/DZ13C.c
@@ -1,13 +1,11 @@
 #include "DZ13C.h"
 
-#define WIDTH 100
-#define HEIGHT 100
+#define HEIGHT 20
 
-int main()
-{
+int main() {
 	int size = WriteSize();
 
-	double *originalSinus =  malloc(size * sizeof(double));
+	double *originalSinus = malloc(size * sizeof(double));
 
 	FillArrSinus(size, originalSinus);
 
@@ -17,38 +15,23 @@ int main()
 
 	FillArrNoiseSinus(size, originalSinus, noiseSinus);
 
-	PrintConsole(size, originalSinus,"Base SinusArr");
-	PrintConsole(size, noiseSinus,"Noise SinusArr");
+	PrintConsole(size, originalSinus, "Base SinusArr");
+	PrintConsole(size, noiseSinus, "Noise SinusArr");
 
-	/*for (int y = HEIGHT; y >= 0; y--)
-	{
-		for (int x = 0; x < size; x++)
-		{
-			if ((int) (signal[x] * (HEIGHT / 2) + HEIGHT / 2) == y)
-			{
-				printf("*");
-			}
-			else
-			{
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}*/
+	PrintConsoleGraph(size, originalSinus);
+	PrintConsoleGraph(size, noiseSinus);
 
 	free(originalSinus);
 	free(noiseSinus);
 	return 0;
 }
 
-int WriteSize()
-{
+int WriteSize() {
 	int tempSize;
 	printf("Write size arr: ");
 	scanf("%d", &tempSize);
 
-	if (tempSize <= 0)
-	{
+	if (tempSize <= 0) {
 		printf("Error size arr\n ");
 		WriteSize();
 	}
@@ -56,36 +39,58 @@ int WriteSize()
 	return tempSize;
 }
 
-void FillArrSinus(int size, double *arr)
-{
+void FillArrSinus(int size, double *arr) {
 	double temp;
-	for (int i = 0; i < size; i++)
-	{
+	for (int i = 0; i < size; i++) {
 		temp = (double) i / size * 2 * M_PI;
 		arr[i] = sin(temp);
 	}
 }
 
-void FillArrNoiseSinus(int size, double *arr, double *noiseArr)
-{
+void FillArrNoiseSinus(int size, double *arr, double *noiseArr) {
 	double noiseTemp;
-	for (int i = 0; i < size; i++)
-	{
-		noiseTemp = (1 + rand() % (10 - 1))*0.01;
+	for (int i = 0; i < size; i++) {
+		noiseTemp = (1 + rand() % (10 - 1)) * 0.01;
 		noiseArr[i] = arr[i] + noiseTemp;
 	}
 }
 
-void PrintConsole(int size, double *arr, char *str)
-{
-	if (str == NULL)
-	{
+void PrintConsole(int size, double *arr, char *str) {
+	if (str == NULL) {
 		str = "NoStr";
 	}
 	printf("\nArr: %s\n", str);
 
-	for (int i = 0; i < size; i++)
-	{
+	for (int i = 0; i < size; i++) {
 		printf("%f\n", arr[i]);
+	}
+}
+
+void PrintConsoleGraph(int size, double *arr)
+{
+	int t = size / 2;
+
+	for (int y = HEIGHT; y >= -HEIGHT; y--)
+	{
+		for (int x = 0; x <= size; x++)
+		{
+			if (y == (int) (arr[x] * HEIGHT))
+			{
+				printf("+");
+			}
+			else if (y == 0)
+			{
+				printf("-"); // X
+			}
+			else if (x == t)
+			{
+				printf("|"); // Y
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
 	}
 }
